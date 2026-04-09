32491ec3791c2562de3e4295c958ad1134c16134
Artem <artem@deffun.io>
2026-04-09 22:56:49 +0700

    save user

--- app/models.py
+++ app/models.py
@@ -1,5 +1,25 @@
+from datetime import datetime, UTC
+from typing import Optional
+
+from sqlalchemy import String, DateTime
 from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
 
 
 class Base(DeclarativeBase):
     pass
+
+
+class User(Base):
+    __tablename__ = "users"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str] = mapped_column(String(30))
+    fullname: Mapped[Optional[str]]
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=lambda: datetime.now(UTC),
+    )
+
+    def __repr__(self) -> str:
+        return f"User(id={self.id!r}, email={self.email!r}, fullname={self.fullname!r})"
